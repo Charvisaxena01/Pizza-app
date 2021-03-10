9c3c64b2c689e960246ec81ea0b2e034841e555c
Charvisaxena01 <74494231+Charvisaxena01@users.noreply.github.com>
2021-03-10 16:48:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+var add = [];
+
+function none() {
+    document.getElementById("display_menu").style.display = "none";
+}
+
+function getmenu() {
+    document.getElementById("display_menu").style.display = "inline-block";
+    document.getElementById("but").style.display = "none";
+}
+debugger
+i = 5.;
+
+function add_item() {
+
+    i++;
+    document.getElementById("no").innerHTML = i;
+    console.log(i);
+    var ad_piz = document.getElementById("add_item").value;
+
+
+    add.push(ad_piz);
+    document.getElementById("display_menu").innerHTML = document.getElementById("display_menu").innerHTML + ad_piz;
+}
